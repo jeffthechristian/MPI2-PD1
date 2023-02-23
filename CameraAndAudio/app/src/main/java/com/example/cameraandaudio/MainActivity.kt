f527b59b1d378aa4dd5d0a58e258e3479c7c72ba
Krista <kristabriede01@gmail.com>
2023-02-23 13:33:25 +0200

    add premissions

--- CameraAndAudio/app/src/main/java/com/example/cameraandaudio/MainActivity.kt
+++ CameraAndAudio/app/src/main/java/com/example/cameraandaudio/MainActivity.kt
@@ -14,21 +14,38 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-     //   get_premissions()
+        get_premissions()
     }
-  //  fun get_premissions(){
-      //  var permissionList = mutableListOf<String>()
+   fun get_premissions(){
+        var permissionList = mutableListOf<String>()
 
-      //  if(checkSelfPermission(android.Manifest.permission.CAMERA)!= PackageManager.PERMISSION_GRANTED)
-      //      permissionList.add(android.Manifest.permission.CAMERA)
-      //  if(checkSelfPermission(android.Manifest.permission.READ_EXTERNAL_STORAGE)!= PackageManager.PERMISSION_GRANTED)
-       //     permissionList.add(android.Manifest.permission.READ_EXTERNAL_STORAGE)
-      //  if(checkSelfPermission(android.Manifest.permission.WRITE_EXTERNAL_STORAGE)!= PackageManager.PERMISSION_GRANTED)
-     //       permissionList.add(android.Manifest.permission.WRITE_EXTERNAL_STORAGE)
+       if(checkSelfPermission(android.Manifest.permission.CAMERA)!= PackageManager.PERMISSION_GRANTED)
+            permissionList.add(android.Manifest.permission.CAMERA)
+      if(checkSelfPermission(android.Manifest.permission.READ_EXTERNAL_STORAGE)!= PackageManager.PERMISSION_GRANTED)
+            permissionList.add(android.Manifest.permission.READ_EXTERNAL_STORAGE)
+        if(checkSelfPermission(android.Manifest.permission.WRITE_EXTERNAL_STORAGE)!= PackageManager.PERMISSION_GRANTED)
+            permissionList.add(android.Manifest.permission.WRITE_EXTERNAL_STORAGE)
 
+        if(permissionList.size>0){
+            requestPermissions(permissionList.toTypedArray(), 101)
+        }
 
+    }
 
-    //}
+    override fun onRequestPermissionsResult(
+        requestCode: Int,
+        permissions: Array<out String>,
+        grantResults: IntArray
+    ) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults)
+
+        grantResults.forEach {
+            if (it != PackageManager.PERMISSION_GRANTED){
+                get_premissions()
+            }
+        }
+    }
+
     override fun onCreateOptionsMenu(menu: Menu?): Boolean {
         menuInflater.inflate(R.menu.menu, menu)
         return true
