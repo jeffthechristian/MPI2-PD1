05fa227b86a917053b7a12a061e3fcf5af1b0376
Krista <kristabriede01@gmail.com>
2023-02-23 14:25:41 +0200

    camera preview

--- CameraAndAudio/app/src/main/java/com/example/cameraandaudio/MainActivity.kt
+++ CameraAndAudio/app/src/main/java/com/example/cameraandaudio/MainActivity.kt
@@ -1,27 +1,82 @@
 package com.example.cameraandaudio
 
+import android.Manifest
+import android.annotation.SuppressLint
+import android.content.Context
 import android.content.Intent
 import android.content.pm.PackageManager
 import android.graphics.SurfaceTexture
+import android.hardware.camera2.CameraCaptureSession
+import android.hardware.camera2.CameraDevice
+import android.hardware.camera2.CameraManager
+import android.hardware.camera2.CaptureRequest
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.os.Handler
+import android.os.HandlerThread
 import android.view.Menu
 import android.view.MenuItem
+import android.view.Surface
 import android.view.TextureView
+import androidx.core.app.ActivityCompat
 
+@Suppress("DEPRECATION")
 class MainActivity : AppCompatActivity() {
+
+    lateinit var capReq: CaptureRequest.Builder
+    lateinit var handler: Handler
+    lateinit var handlerThread: HandlerThread
+    lateinit var cameraManager: CameraManager
+    lateinit var textureView: TextureView
+    lateinit var cameraCaptureSession: CameraCaptureSession
+    lateinit var cameraDevice: CameraDevice
+    lateinit var captureRequest: CaptureRequest
+
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-        get_premissions()
+        getPremissions()
+
+
+        textureView = findViewById(R.id.textureView)
+        cameraManager = getSystemService(Context.CAMERA_SERVICE) as CameraManager
+
+        handlerThread = HandlerThread("Thread")
+        handlerThread.start()
+        handler = Handler((handlerThread).looper)
+        
+        
+        textureView.surfaceTextureListener = object : TextureView.SurfaceTextureListener {
+            override fun onSurfaceTextureAvailable(
+                surface: SurfaceTexture,
+                width: Int,
+                height: Int
+            ) {
+                openCamera()
+            }
+
+            override fun onSurfaceTextureSizeChanged(
+                surface: SurfaceTexture,
+                width: Int,
+                height: Int
+            ) {
+            }
+
+            override fun onSurfaceTextureDestroyed(surface: SurfaceTexture): Boolean {
+                return false
+            }
+
+            override fun onSurfaceTextureUpdated(surface: SurfaceTexture) {
+            }
+        }
     }
-   fun get_premissions(){
+  private fun getPremissions(){
         var permissionList = mutableListOf<String>()
 
-       if(checkSelfPermission(android.Manifest.permission.CAMERA)!= PackageManager.PERMISSION_GRANTED)
+        if(checkSelfPermission(android.Manifest.permission.CAMERA)!= PackageManager.PERMISSION_GRANTED)
             permissionList.add(android.Manifest.permission.CAMERA)
-      if(checkSelfPermission(android.Manifest.permission.READ_EXTERNAL_STORAGE)!= PackageManager.PERMISSION_GRANTED)
+        if(checkSelfPermission(android.Manifest.permission.READ_EXTERNAL_STORAGE)!= PackageManager.PERMISSION_GRANTED)
             permissionList.add(android.Manifest.permission.READ_EXTERNAL_STORAGE)
         if(checkSelfPermission(android.Manifest.permission.WRITE_EXTERNAL_STORAGE)!= PackageManager.PERMISSION_GRANTED)
             permissionList.add(android.Manifest.permission.WRITE_EXTERNAL_STORAGE)
@@ -41,11 +96,58 @@ class MainActivity : AppCompatActivity() {
 
         grantResults.forEach {
             if (it != PackageManager.PERMISSION_GRANTED){
-                get_premissions()
+                getPremissions()
             }
         }
     }
 
+
+
+    fun openCamera(){
+        if (ActivityCompat.checkSelfPermission(
+                this,
+                Manifest.permission.CAMERA
+            ) != PackageManager.PERMISSION_GRANTED
+        ) {
+            // TODO: Consider calling
+            //    ActivityCompat#requestPermissions
+            // here to request the missing permissions, and then overriding
+            //   public void onRequestPermissionsResult(int requestCode, String[] permissions,
+            //                                          int[] grantResults)
+            // to handle the case where the user grants the permission. See the documentation
+            // for ActivityCompat#requestPermissions for more details.
+            return
+        }
+        cameraManager.openCamera(cameraManager.cameraIdList[0], object : CameraDevice.StateCallback(){
+            override fun onOpened(camera: CameraDevice) {
+                cameraDevice = camera
+
+                capReq = cameraDevice.createCaptureRequest(CameraDevice.TEMPLATE_PREVIEW)
+
+                val surface = Surface(textureView.surfaceTexture)
+                capReq.addTarget(surface)
+
+                cameraDevice.createCaptureSession(listOf(surface), object :
+                    CameraCaptureSession.StateCallback() {
+                    override fun onConfigured(session: CameraCaptureSession) {
+                        cameraCaptureSession = session
+                        cameraCaptureSession.setRepeatingRequest(capReq.build(), null,null)
+                    }
+
+                    override fun onConfigureFailed(session: CameraCaptureSession) {
+                    }
+
+
+                },handler)
+            }
+
+            override fun onDisconnected(camera: CameraDevice) {
+            }
+
+            override fun onError(camera: CameraDevice, error: Int) {
+            }
+        }, handler)
+    }
     override fun onCreateOptionsMenu(menu: Menu?): Boolean {
         menuInflater.inflate(R.menu.menu, menu)
         return true
